285843c156fa886fb3f9908025fe96836f817a96
xXFacundo <facundomendoza2405@gmail.com>
2017-01-04 19:11:51 -0300

    Pruebas con animaciones CSS3

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <html>
 <head>
   <?php
-    include("classes.php");
+    include("classes/DBManager.php");
    ?>
+   <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
   <title>AOV</title>
 </head>
 <body>
   <?php
-    $connection = new db("localhost","root","","aov");
+    $connection = new DBManager("localhost","root","","aov");
    ?>
+   <div class="boton">Test</div>
 </body>
 </html>
